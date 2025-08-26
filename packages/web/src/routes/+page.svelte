582820c3c459958dfbc15529e84c8a815c9d6010
kimseungbin <luxorienskim@gmail.com>
2025-08-26 10:07:32 +0900

    feat(UI): Add meta description to +page.svelte for better SEO

--- packages/web/src/routes/+page.svelte
+++ packages/web/src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <svlte:head>
     <title>Trip Settle</title>
+    <meta content="Trip Settle - settle travel costs effortlessly." name="description"/>
 </svlte:head>
 <main>
     <h1>Hello SvelteKit!</h1>
